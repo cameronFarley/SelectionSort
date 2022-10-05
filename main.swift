4003887f967ad74b4bcd0ffa26785d909857e9a0
Cameron Farley <cameron.farley@student.allenisd.org>
2022-10-05 16:57:26 -0500

    updated selection sort, no print statement

--- main.swift
+++ main.swift
@@ -10,8 +10,6 @@ let unsortedIntegers = [5, 1, 4, 2, 8]
 let STDIN = (readLine(strippingNewline: true))?.split {$0 == " "}.map (String.init)
 
 func selectionSort(_ array: [String]) {
-    var pass = 0
-    var swapTotal = 0
     var a = array
     for x in 0..<a.count - 1 {
         var min_value = x
@@ -24,11 +22,7 @@ func selectionSort(_ array: [String]) {
         if x != min_value {
             a.swapAt(x, min_value)
         }
-        pass += 1
-        swapTotal += 1
-        print("Pass: \(pass), Swaps: 1/\(swapTotal), Array: \(a)")
     }
 }
 
-print("Pass: 0, Swaps: 0/0, Array: \(STDIN!)")
 selectionSort(STDIN!)
